9a8bf69259f384ee67c237d461cc229d5e022e83
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-02-03 16:50:26 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <section className="py-24">
       <div className="container">
-        <h1 className="text-3xl font-bold"> Next TS Starter Template </h1>
+        <h1 className="text-3xl font-bold"> Next TS Starter Template - for starter </h1>
 
       </div>
 
